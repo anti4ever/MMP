76d5ede1621c07338a087a05562a657b116633e2
Anton <anti4ever@mail.ru>
2018-11-05 22:34:11 +0500

    Update program.py

---
+++ program.py
@@ -0,0 +1,101 @@
+from matplotlib import mlab
+from terminaltables import AsciiTable
+import math, random, numpy
+import matplotlib.pyplot as plt
+
+
+
+a0 = 0.057 # Внутренний радиус НКТ, м
+a1 = 0.1095 # Наружный радиус НКТ с учетом теплоизоляции, м
+### a2 = # 
+a = 0.2 # Наружный радиус цементного кольца за внешней колонной скважины, м
+#### l_izol = # Протяженность теплоизолированного участка, м
+#### lambda_izol = # Коэффициент теплопроводности слоя теплоизоляции НКТ, Вт/(м*град)
+lambda_gp = 0.52 # Коэффициент теплопроводности газовой прослойкимежду НКТ и экспл. колонной, Вт/(м*град)
+lambda_cem = 0.6 # Коэффициент теплопроводности цементного камня, Вт/(м*град)
+t0 = -4 # Среднегодовая температура поверхности массива мерзлых пород, град. цельсия
+tf = 30 # Температура добываемого флюида, град. цкльсия
+C = 0.5772
+
+h = 50 #Мощность расчитываемого интервала, м
+
+#### Задается для каждого расчетного интервала:
+
+z = 50 # Мощность слоя
+
+plotn_sk = 1800 # плотность скелета мерзлой породы, кг/м3
+w_tot = 0.3 # Сумарная весовая влажность мерзлой породы, доля единиц (д.е.)
+w_w = 0 # Весовая влажность мерзлой породы за счет незамерзшей воды (д.е)
+lambda_t = 1.4 # Коэффициент теплопроводности пород в талом состоянии, Вт/(м*град)
+lambda_m = 1.8 # Коэффициент теплопроводности пород в мерзлом состоянии, Вт/(м*град)
+tfi = 0 # Температура начала оттаивания мерзлых пород, град. цкльсия / Температура начала фазовых переходов
+
+tau = 30 # Время эксплуатации, годы
+speed = 0.001 #Точность вычисления / скорость
+
+class Layer(object):
+    def __init__(self, z, plotn_sk, w_tot, w_w, lambda_t, lambda_m, tfi):
+        self.z = z
+        self.plotn_sk = plotn_sk
+        self.w_tot = w_tot
+        self.w_w = w_w
+        self.lambda_t = lambda_t
+        self.lambda_m = lambda_m
+        self.tfi = tfi
+        self.lambda_ef = ((1 / (((1/lambda_gp)*math.log((a1/a0),math.e)+((1/lambda_cem)*math.log((a/a1),math.e)))))*math.log((a/a0),math.e))
+        self.ps_i = (math.log((a/a0),math.e))/(math.log((2*h/a),math.e))
+        self.tc = tf*((1+(lambda_m*t0*self.ps_i/self.lambda_ef/tf))/(1+(self.lambda_t*self.ps_i/self.lambda_ef)))
+        self.beta = -1*(self.lambda_m*(t0-self.tfi))/(self.lambda_t*(self.tc-self.tfi))
+        self.alfa = (335000 * self.plotn_sk * (self.w_tot-self.w_w)) / (31100000 * self.lambda_t * (self.tc - self.tfi))
+
+    def info(self):
+        table_data = [
+        ['H слоя, м', 'plotn_sk', 'w_tot', 'w_w', 'lambda_t', 'lanbda_m','tfi'],
+        [str(self.z), str(self.plotn_sk), str(self.w_tot), str(self.w_w), str(self.lambda_t), str(self.lambda_m), str(self.tfi)],    
+        ]
+        table = AsciiTable(table_data)
+        table.title = 'Layer 1'
+        print (table.table)
+    
+    def r_r(self,z):
+        z = z
+        rl=a*(math.pow((2*z/a),(1/(1+self.beta))))
+        print('rl:',z,rl)
+        r = a + 0.01
+        tau0 = 0
+        d_frz_0 = -1 * (1-(C/math.log((2*z/a),math.e)))/(r*math.log((2*z/a),math.e))
+        frz_0 = 1 - ((math.log((r/a),math.e) / (math.log((2*z/a),math.e))) * (1 - (C / (math.log((2*z/a),math.e)))))
+        int_0 = self.alfa * (1 / (d_frz_0 * (1/(frz_0-1)+self.beta/frz_0)))
+        while tau >= tau0:
+            d_frz = -1 * (1-(C/math.log((2*z/a),math.e)))/(r*math.log((2*z/a),math.e))
+            frz = 1 - ((math.log((r/a),math.e) / (math.log((2*z/a),math.e))) * (1 - (C / (math.log((2*z/a),math.e)))))
+            int_1 = self.alfa *  (1 / (d_frz * (1/(frz-1)+self.beta/frz)))
+            tau_d = ((int_0+int_1)/2) * speed #Точность вычисления / скорость
+            int_0 = int_1
+            r = r + speed #Точность вычисления / скорость
+            tau0 = tau0 + tau_d
+            if r >= rl:
+                r = rl
+                break
+        return r
+   
+    def mas_rr(self):
+        i = 1
+        mas_rad = []
+        while i <= self.z:
+            mas_rad.append(self.r_r(i))
+            print(mas_rad[i-1]) 
+            i = i+1
+        return mas_rad
+
+Layer1 = Layer(z,plotn_sk,w_tot,w_w,lambda_t,lambda_m,tfi)
+
+mas_plot_rr = Layer1.mas_rr()
+
+plot_z = numpy.arange(1,z+1)
+
+plt.grid(True)
+plt.gca().invert_yaxis()
+plt.plot(mas_plot_rr,plot_z)
+Layer1.info()
+plt.show()
